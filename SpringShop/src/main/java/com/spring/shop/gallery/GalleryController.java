6212a4249388153a4bb85a6c158687f882c19ac2
User <User@DESKTOP-N2TRNVD>
2022-01-07 17:06:11 +0900

    test

--- SpringShop/src/main/java/com/spring/shop/gallery/GalleryController.java
+++ SpringShop/src/main/java/com/spring/shop/gallery/GalleryController.java
@@ -12,9 +12,8 @@ public class GalleryController {
 	@RequestMapping(value = "/gallery", method = RequestMethod.GET)
 	public String gallery(HttpServletRequest req) {
 		req.setAttribute("content", "gallery.jsp");
+		System.out.println("¾Æ¸¶µµ?");
 		return "home";
 	}
-	
-	
 	
 }
